d91ba184ab442b4f5f44cdb7a2efa77529c11041
我妻 晴香 <200646@jc-21.jp>
2023-06-08 12:18:51 +0900

    syusei

--- MyBootApp2/src/main/java/jp/te4a/spring/boot/myapp2/HelloController.java
+++ MyBootApp2/src/main/java/jp/te4a/spring/boot/myapp2/HelloController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 public class HelloController
 {
-	@RequestMapping("/")
+	@RequestMapping("/taro")
 	public String index() {
 		return "Taro desu!";
 	}
